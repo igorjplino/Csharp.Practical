40ff639a555dc5fdf41047bfeb98a6fc6cbc3db2
Igor Jorge <igorjplino@gmail.com>
2020-04-15 20:05:15 -0300

    andamento da parte 4 do curso

---
+++ ByteBank.4/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(456, 321);
+                ContaCorrente conta2 = new ContaCorrente(897, 3541521);
+
+                conta.Transferir(500, conta2);
+
+                Console.WriteLine($"Saldo atual: {conta.Saldo}");
+
+                conta.Sacar(200);
+            }
+            catch (SaldoInsuficienteException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Exceção de saldo insuficiente");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Argumento com problema: " + ex.ParamName);
+                Console.WriteLine("Ocorreu uma exceção do tipo ArgumentException");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            //Metodo();
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair");
+            Console.ReadLine();
+        }
+
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Exceção com numero=" + numero + " e divisor=" + divisor);
+                throw;
+                Console.WriteLine("Código depois do throw");
+            }
+        }
+
+    }
+}
