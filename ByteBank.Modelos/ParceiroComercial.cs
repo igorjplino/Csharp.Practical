c5f15e5340655d716386c47924229d23936642f3
Igor Jorge <igorjplino@gmail.com>
2020-04-17 07:27:43 -0300

    Iniciando o curso csharp pt 5

---
+++ ByteBank.Modelos/ParceiroComercial.cs
@@ -0,0 +1,17 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBank.Modelos
+{
+    public class ParceiroComercial : IAutenticavel
+    {
+        public string Senha { get; set; }
+
+        public bool Autenticar(string senha)
+        {
+            return Senha == senha;
+        }
+    }
+}
