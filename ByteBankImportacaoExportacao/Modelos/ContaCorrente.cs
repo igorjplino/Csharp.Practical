53d1a3998129a5d9b953dfba051c8d3594f4e1d4
Igor Jorge <igorjplino@gmail.com>
2020-05-14 20:25:42 -0300

    Iniciando curso csharp pt9

---
+++ ByteBankImportacaoExportacao/Modelos/ContaCorrente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBankImportacaoExportacao.Modelos
+{
+    public class ContaCorrente
+    {
+        public int Numero { get; }
+        public int Agencia { get; }
+        public double Saldo { get; private set; }
+        public Cliente Titular { get; set; }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+        }
+    }
+}
