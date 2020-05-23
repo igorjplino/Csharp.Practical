10ea7dc776cadd43b491891da35c1e6398e32a75
Igor Jorge <igorjplino@gmail.com>
2020-05-23 11:10:27 -0300

    Curso csharp Collections

---
+++ Curso.Collections/Parte1/ListaSomenteLeitura/Curso.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaSomenteLeitura
+{
+    class Curso
+    {
+        private IList<Aula> _aulas;
+
+        public Curso(string nome, string instrutor)
+        {
+            Nome = nome;
+            Instrutor = instrutor;
+
+            _aulas = new List<Aula>();
+        }
+
+        /// <summary>
+        /// Adicionar aula no curso
+        /// </summary>
+        /// <param name="aula">Aula a ser adicionada</param>
+        /// <exception cref="ArgumentNullException">Exceção lançada quando o argumento <paramref name="aula"/> é nulo.</exception>
+        /// <exception cref="ArgumentException">Exceção lançada quando a Aula já existe no Curso.</exception>
+        internal void AdicionarAula(Aula aula)
+        {
+            if (aula == null)
+            {
+                throw new ArgumentNullException(nameof(aula), "Não é permitido adicionar uma aula Nula");
+            }
+
+            if (_aulas.Any(o => o.Titulo.Equals(aula.Titulo, StringComparison.OrdinalIgnoreCase)))
+            {
+                throw new ArgumentException($"Aula '{aula.Titulo}' já existente no curso", nameof(aula));
+            }
+
+            _aulas.Add(aula);
+        }
+
+        public string Nome { get; set; }
+        public string Instrutor { get; set; }
+        public IList<Aula> Aulas 
+        {
+            get
+            {
+                return new ReadOnlyCollection<Aula>(_aulas);
+            }
+        }
+    }
+}
---
+++ Curso.Collections/Parte1/ListaSomenteLeitura/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaSomenteLeitura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var curso = new Curso("Estudando CSharp", "Igor Jorge");
+
+            curso.AdicionarAula(new Aula("Lista somente leitura", 30));
+            
+            Imprimir(curso.Aulas);
+        }
+
+        private static void Imprimir(IList<Aula> aulas)
+        {
+            Console.Clear();
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
