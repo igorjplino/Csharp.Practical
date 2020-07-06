44c03d0529c5e59cdbb1623df5671ce241f77bcc
Igor Jorge <igorjplino@gmail.com>
2020-07-06 20:09:55 -0300

    Iniciando curso Csharp novidades da versão 7.3

---
+++ csharp73/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using csharp73.Aula1;
+
+namespace csharp73
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TesteTuples.MaisAlgunsCasos();
+
+            Console.WriteLine("Final da execução!");
+        }
+    }
+}
